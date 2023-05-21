f0679cdb87ee69d5dc9a0f1bc4eac2d7a5b0785f
kwamewiilder <oheneba.ega@outlook.com>
2023-05-21 03:07:03 +0000

    get added

--- controllers/region.js
+++ controllers/region.js
@@ -26,3 +26,38 @@ exports.storeRegion = async (req, res) => {
     });
   }
 };
+
+exports.getUsersByRegion = async (req, res) => {
+  try {
+    const regionName = req.params.regionName;
+    const users = await User.find({ region: regionName });
+
+    if (users.length === 0) {
+      return res.status(404).send({ message: 'No users found for the specified region' });
+    }
+
+    return res.send(users);
+  } catch (err) {
+    return res.status(500).send({
+      message: 'Error retrieving users by region',
+      error: err
+    });
+  }
+};
+
+exports.getAllUsers = async (req, res) => {
+  try {
+    const users = await User.find();
+
+    if (users.length === 0) {
+      return res.status(404).send({ message: 'No users found' });
+    }
+
+    return res.send(users);
+  } catch (err) {
+    return res.status(500).send({
+      message: 'Error retrieving all users',
+      error: err
+    });
+  }
+};
