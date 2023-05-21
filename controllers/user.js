d480234a63982a29b32ff689316fa2be8cbe18b3
kwamewiilder <oheneba.ega@outlook.com>
2023-05-21 00:41:24 +0000

    bug fixed 🥳

--- controllers/user.js
+++ controllers/user.js
@@ -1,23 +1,35 @@
 const db = require('../models');
 const User = db.user;
+const Region = db.region;
 
 exports.create = async (req, res) => {
   try {
     // Validate request
-    if (!req.body.username || !req.body.password) {
+    if (!req.body.username || !req.body.password || !req.body.region) {
       return res.status(400).send({ message: 'Content can not be empty!' });
     }
 
+    const { username, region } = req.body;
+
     const user = new User(req.body);
-    const data = await user.save();
-    console.log(data);
-    return res.status(201).send(data);
+    const savedUser = await user.save();
+
+    // Add the username to the region's list of usernames
+    const regionData = await Region.findOneAndUpdate(
+      { regionName: region },
+      { $addToSet: { usernames: username } },
+      { upsert: true }
+    );
+
+    console.log(savedUser);
+    return res.status(201).send(savedUser);
   } catch (err) {
     return res.status(500).send({
       message: err.message || 'Some error occurred while creating the user.'
     });
   }
 };
+
 
 exports.getAll = async (req, res) => {
   try {
@@ -38,6 +50,31 @@ exports.getUser = async (req, res) => {
   } catch (err) {
     return res.status(500).send({
       message: err.message || 'Some error occurred while retrieving users.'
+    });
+  }
+};
+
+exports.deleteUser = async (req, res) => {
+  try {
+    const username = req.params.username;
+    const data = await User.deleteOne({ username: username });
+    return res.send(data);
+  } catch (err) {
+    return res.status(500).send({
+      message: err.message || 'Some error occurred while deleting the user.'
+    });
+  }
+};
+
+exports.updateUser = async (req, res) => {
+  try {
+    const username = req.params.username;
+    const updates = req.body;
+    const data = await User.findOneAndUpdate({ username: username }, updates, { new: true });
+    return res.send(data);
+  } catch (err) {
+    return res.status(500).send({
+      message: err.message || 'Some error occurred while updating the user.'
     });
   }
 };
