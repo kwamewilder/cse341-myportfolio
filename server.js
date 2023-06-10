5e8df0859aa0052174bb12a160b9ba9ca3b1fd10
kwamewiilder <oheneba.ega@outlook.com>
2023-06-10 23:19:48 +0000

    AUTH0

--- server.js
+++ server.js
@@ -5,6 +5,26 @@ const bodyParser = require('body-parser');
 const port = process.env.PORT || 8080;
 const app = express();
 
+const { auth } = require('express-openid-connect');
+
+const config = {
+  authRequired: false,
+  auth0Logout: true,
+  secret: 'a long, randomly-generated string stored in env',
+  baseURL: 'https://cse341-profile.onrender.com',
+  clientID: 'jjAammrRu0b42FQUqbfW9dm0Lo6aJ61Z',
+  issuerBaseURL: 'https://dev-dsvwpop7ugtq47ag.us.auth0.com'
+};
+
+// auth router attaches /login, /logout, and /callback routes to the baseURL
+app.use(auth(config));
+
+// req.isAuthenticated is provided from the auth router
+app.get('/', (req, res) => {
+  res.send(req.oidc.isAuthenticated() ? 'Logged in' : 'Logged out');
+});
+
+
 app.use(bodyParser.json());
 app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
