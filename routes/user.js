c7d3ca05fa620fba0b617c4025f57df808ba7d98
kwamewiilder <oheneba.ega@outlook.com>
2023-06-10 23:37:42 +0000

    user requires auth

--- routes/user.js
+++ routes/user.js
@@ -2,11 +2,16 @@ const express = require('express');
 const router = express.Router();
 const app = express();
 
-
-
 const userController = require('../controllers/user');
-
 const { auth, requiresAuth } = require('express-openid-connect');
+
+// Middleware to check if the user is authenticated
+const requireLogin = (req, res, next) => {
+  if (req.oidc.isAuthenticated()) {
+    return next();
+  }
+  res.redirect('/login'); // Redirect to login page if not authenticated
+};
 
 app.use(
   auth({
@@ -22,9 +27,7 @@ app.get('/admin', requiresAuth(), (req, res) =>
 router.get('/', userController.getAll);
 router.get('/:username', userController.getUser);
 router.post('/', userController.create);
-router.put('/:username', userController.updateUser);
-router.delete('/:username', userController.deleteUser);
-
-
+router.put('/:username', requiresAuth(), userController.updateUser); // Require authentication for PUT
+router.delete('/:username', requiresAuth(), userController.deleteUser); // Require authentication for DELETE
 
 module.exports = router;
