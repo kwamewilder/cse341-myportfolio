76baf01bc629170be2475717ec2d6d64009ab723
kwamewiilder <oheneba.ega@outlook.com>
2023-06-10 23:30:02 +0000

    require auth

--- routes/user.js
+++ routes/user.js
@@ -1,10 +1,23 @@
 const express = require('express');
 const router = express.Router();
+const app = express();
 
 
 
 const userController = require('../controllers/user');
 
+const { auth, requiresAuth } = require('express-openid-connect');
+
+app.use(
+  auth({
+    authRequired: false,
+  })
+);
+
+// requiresAuth checks authentication.
+app.get('/admin', requiresAuth(), (req, res) =>
+  res.send(`Hello ${req.oidc.user.sub}, this is the admin section.`)
+);
 
 router.get('/', userController.getAll);
 router.get('/:username', userController.getUser);
