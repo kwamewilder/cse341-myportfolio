fc59014893fbeb5479d7d018025f2f8e8a9a2ac2
kwamewiilder <oheneba.ega@outlook.com>
2023-06-11 00:01:36 +0000

    swagger.json updated

--- routes/region.js
+++ routes/region.js
@@ -1,8 +1,21 @@
-
 const express = require('express');
 const router = express.Router();
 const db = require('../models');
 const Region = db.region;
+const { requiresAuth } = require('express-openid-connect');
+
+// Middleware to check if the user is authenticated
+const requireLogin = (req, res, next) => {
+  if (req.oidc.isAuthenticated()) {
+    return next();
+  }
+  res.redirect('/login'); // Redirect to login page if not authenticated
+};
+
+// requiresAuth checks authentication.
+router.get('/admin', requiresAuth(), (req, res) =>
+  res.send(`Hello ${req.oidc.user.sub}, this is the admin section.`)
+);
 
 // Get regions
 router.get('/', async (req, res) => {
@@ -22,7 +35,7 @@ router.get('/user/region/:regionName', async (req, res) => {
   const { regionName } = req.params;
 
   try {
-    const users = await users.find({ region: regionName });
+    const users = await User.find({ region: regionName });
     res.status(200).json(users);
   } catch (err) {
     res.status(500).json({
@@ -32,5 +45,35 @@ router.get('/user/region/:regionName', async (req, res) => {
   }
 });
 
+// Update region
+router.put('/:regionId', requiresAuth(), async (req, res) => {
+  const { regionId } = req.params;
+  const updatedRegion = req.body;
+
+  try {
+    const region = await Region.findByIdAndUpdate(regionId, updatedRegion, { new: true });
+    res.status(200).json(region);
+  } catch (err) {
+    res.status(500).json({
+      message: 'Error updating region',
+      error: err
+    });
+  }
+});
+
+// Delete region
+router.delete('/:regionId', requiresAuth(), async (req, res) => {
+  const { regionId } = req.params;
+
+  try {
+    await Region.findByIdAndDelete(regionId);
+    res.status(200).json({ message: 'Region deleted successfully' });
+  } catch (err) {
+    res.status(500).json({
+      message: 'Error deleting region',
+      error: err
+    });
+  }
+});
 
 module.exports = router;
